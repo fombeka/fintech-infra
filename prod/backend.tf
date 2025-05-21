5eab0cb364a64b527c200d33ed18a3b2552dd82f
fombeka <fombekasandrine@gmail.com>
2025-05-21 02:26:07 +0100

    Update backend.tf

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "sclass38dominion-terraform-backend"
+    bucket         = "class38dominion-terraform-backend"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
