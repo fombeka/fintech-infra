23d3dc4f09b4f695dc807192a79ff71c4890a9f5
fombeka <fombekasandrine@gmail.com>
2025-05-21 03:37:56 +0100

    Update variable.tf

--- modules/acm/variable.tf
+++ modules/acm/variable.tf
@@ -2,19 +2,19 @@
 variable "domain_name" {
   description = "Primary domain name for the certificate"
   type        = string
-  default     = "dominionsystem.org"
+  default     = "sandysoft.org"
 }
 
 variable "san_domains" {
   description = "Subject alternative names for the certificate"
   type        = list(string)
-  default     = ["*.dominionsystem.org"]
+  default     = ["*.sandysoft.org"]
 }
 
 variable "route53_zone_id" {
   description = "Route 53 Hosted Zone ID"
   type        = string
-  default     = "Z05475331ZK00RPD27RX0" # Replace with actual Route 53 Zone ID
+  default     = "Z07425561H5MRYWRI1TK8" # Replace with actual Route 53 Zone ID
 }
 
 variable "tags" {
